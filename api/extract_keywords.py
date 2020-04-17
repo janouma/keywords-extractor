2ed74dc3fa9a06fe2e657c148e246a06cedf5379
Judicaël Anouma <janouma@gmail.com>
2020-04-17 18:40:55 +0200

    use cerberus for request body validation
    improve error handling

--- api/extract_keywords.py
+++ api/extract_keywords.py
@@ -1,21 +1,33 @@
 from http.server import BaseHTTPRequestHandler
-from jsonschema import validate
+from cerberus import Validator
 import json
 from lib.keywords_extractor import KeywordsExtractor
 from lib.words_counter import count_words
 
 body_schema = {
-    'type': 'object',
-    'properties': {
-        'lang': {
-            'type': 'string',
-            'enum': ['en']
-        },
-        'text': {'type': 'string'},
-        'limitRatio': {'type': 'integer'},
-        'max': {'type': 'integer'}
+    'lang': {
+        'type': 'string',
+        'allowed': ['en'],
+        'required': True
+    },
+
+    'text': {
+        'type': 'string',
+        'required': True
+    },
+
+    'limitRatio': {
+        'type': 'integer',
+        'required': True
+    },
+
+    'max': {
+        'type': 'integer',
+        'required': True
     }
 }
+
+validator = Validator(body_schema)
 
 
 class handler(BaseHTTPRequestHandler):
@@ -25,31 +37,44 @@ class handler(BaseHTTPRequestHandler):
         raw_body = self.rfile.read(body_length)
         json_body = json.loads(raw_body)
 
-        validate(json_body, body_schema)
+        is_body_valid = validator.validate(json_body)
 
-        extractor = KeywordsExtractor(json_body['lang'])
+        if is_body_valid is True:
+            extractor = KeywordsExtractor(json_body['lang'])
 
-        extractor.analyze(
-            json_body['text'],
-            candidate_pos=['NOUN', 'PROPN', 'VERB'],
-            window_size=4,
-            lower=True
-        )
+            extractor.analyze(
+                json_body['text'],
+                candidate_pos=['NOUN', 'PROPN', 'VERB'],
+                window_size=4,
+                lower=True
+            )
 
-        keywords = extractor.get_keywords(
-            percent=json_body['limitRatio'],
-            max_keywords=json_body['max']
-        )
+            keywords = extractor.get_keywords(
+                percent=json_body['limitRatio'],
+                max_keywords=json_body['max']
+            )
 
-        words_count = count_words(json_body['text'])
+            words_count = count_words(json_body['text'])
 
-        response_body = json.dumps({
-            'keywords': keywords,
-            'wordsCount': words_count
-        })
+            response_body = json.dumps({
+                'keywords': keywords,
+                'wordsCount': words_count
+            })
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(response_body)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(response_body.encode())
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            response_body = json.dumps({
+                'message': 'body validation failed',
+                'errors': validator.errors
+            })
+
+            self.wfile.write(response_body.encode())
+
         return
